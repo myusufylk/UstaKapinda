fee9a202febe72084636e93fb9e837127106ba79
Yusuf Yılıkoğlu <yusuf.y0079@icloud.com>
2025-05-31 14:05:40 +0300

    chatbot çalışması yapıldı

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// Basit bir kategori listesi endpoint'i
+// Basit bir kullanıcı listesi endpoint'i
 router.get('/', (req, res) => {
-  res.json({ message: 'Kategoriler endpointi çalışıyor.' });
+  res.json({ message: 'Kullanıcılar endpointi çalışıyor.' });
 });
 
 module.exports = router; 
